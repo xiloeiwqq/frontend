2fddba71cbf1481cd293f70b9234d24713ca1273
xiloeiwqq <pspsibladessss@gmail.com>
2025-12-08 17:37:44 +0300

    forgot about js

---
+++ script.js
@@ -0,0 +1,64 @@
+//task1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+//task2
+
+let iPhoneRelease = 2007;
+alert(iPhoneRelease);
+
+//task3
+
+let jsCreator = 'Brendan Eich';
+alert(jsCreator);
+
+//task4
+
+let num1 = 10;
+let num2 = 2;
+alert(num1 + num2);
+alert(num1 - num2);
+alert(num1 * num2);
+alert(num1 / num2);
+
+//task5
+
+let result = num2 ** 5;
+alert(result)
+
+//task6
+
+let n1 = 9;
+let n2 = 2;
+alert(n1 % n2)
+
+//task7
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);
+
+//task8
+
+let age = prompt('How old are you?')
+alert(age)
+
+//task9
+
+let user = {
+    name: 'Костик',
+    age: 15,
+    isAdmin: true
+};
+
+//task10
+
+let userName = prompt('Your name: ')
+alert('Hi, ${userName}!')
