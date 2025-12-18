a7be50a22b5aabca567e1aaa278557b57853e566
xiloeiwqq <pspsibladessss@gmail.com>
2025-12-18 21:10:30 +0300

    небольшие доработки

--- script.js
+++ script.js
@@ -1,38 +1,80 @@
-console.log("Привет");
-console.log("Привет");
-
-for (let i = 1; i <= 5; i++) {
-    console.log(i);
+//task1
+function getMin(a, b) {
+    if (a <= b) {
+        return a;
+    } else {
+        return b;
+    }
 }
 
-for (let i = 7; i <= 22; i++) {
-    console.log(i);
+//task2
+function checkEvenOdd(n) {
+    if (n % 2 == 0) {
+        return 'Число четное';
+    } else {
+        return 'Число нечетное';
+    }
 }
 
-const obj = {
-    "Коля": '200',
-    "Вася": '300',
-    "Петя": '400'
+//task3
+function printSquare(n) {
+    console.log(n * n);
+}
+
+function getSquare(n) {
+    return n * n;
+}
+
+//task4
+function checkAge() {
+    const age = prompt('Сколько вам лет?');
+    const ageNum = Number(age);
+    
+    if (ageNum < 0) {
+        alert('Вы ввели неправильное значение');
+    } else if (ageNum >= 0 && ageNum <= 12) {
+        alert('Привет, друг!');
+    } else if (ageNum >= 13) {
+        alert('Добро пожаловать!');
+    }
+}
+
+//task5
+function multiply(a, b) {
+    if (isNaN(Number(a)) || isNaN(Number(b))) {
+        return 'Одно или оба значения не являются числом';
+    }
+    return Number(a) * Number(b);
+}
+
+//task6
+function getCube() {
+    const input = prompt('Введите число:');
+    const n = Number(input);
+    
+    if (isNaN(n)) {
+        return 'Переданный параметр не является числом';
+    }
+    return n + ' в кубе равняется ' + (n * n * n);
+}
+
+//task7
+const circle1 = {
+    radius: 5,
+    getArea: function() {
+        return Math.PI * this.radius * this.radius;
+    },
+    getPerimeter: function() {
+        return 2 * Math.PI * this.radius;
+    }
 };
 
-for (const key in obj) {
-    console.log(key + " — зарплата " + obj[key] + " долларов");
-}
-
-let n = 1000;
-let num = 0;
-
-while (n >= 50) {
-    n = n / 2;
-    num++;
-}
-
-console.log(n);
-console.log(num);
-
-const firstFriday = 5;
-const daysInMonth = 31;
-
-for (let day = firstFriday; day <= daysInMonth; day += 7) {
-    console.log("Сегодня пятница, " + day + "-е число. Необходимо подготовить отчет.");
-}
+const circle2 = {
+    radius: 10,
+    getArea: function() {
+        return Math.PI * this.radius * this.radius;
+    },
+    getPerimeter: function() {
+        return 2 * Math.PI * this.radius;
+    }
+};
